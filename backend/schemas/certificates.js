b12bf14349b3ff0e29c784d6619c16276b267309
Kebron Solomon Z <keb2@MacBook-Pro.local>
2022-03-31 18:16:41 +0200

    backend sanity schema setup

---
+++ backend/schemas/certificates.js
@@ -0,0 +1,22 @@
+export default {
+  name: "certificates",
+  title: "Certificates",
+  type: "document",
+  fields: [
+    {
+      name: "courseTitle",
+      title: "CourseTitle",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+    },
+    {
+      name: "courseLink",
+      title: "CourseLink",
+      type: "string",
+    },
+  ],
+};
---
+++ backend/schemas/projects.js
@@ -0,0 +1,49 @@
+export default {
+  name: "projects",
+  title: "Projects",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "projectLink",
+      title: "ProjectLink",
+      type: "string",
+    },
+    {
+      name: "codeLink",
+      title: "CodeLink",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [
+        {
+          name: "tag",
+          title: "Tag",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
